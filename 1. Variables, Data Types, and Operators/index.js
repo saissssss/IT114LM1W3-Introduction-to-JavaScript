6507a356242184b10c9d9cbcbab0d130f1b7f4ae
allendmbMCL <113911289+saissssss@users.noreply.github.com>
2024-01-13 09:14:20 +0000

    Update index.js and index.js

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -5,37 +5,53 @@
 
 // TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
 // Your code here
-
+console.log(carName);
+var carName = "Volvo";
 // TODO 1.2 Use the let keyword to define a variable.
 // Your code here
-
+let x = 10;
+console.log(x);
 // TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
 // Your code here
-
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+//alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; won't work
+console.log(alphabet);
 // Checkpoint 1.1 What is the difference between var, let, and const?
-// Answer: 
+// Answer: var can be reassigned and can be used before declaration, let cannot be reassigned and cannot be used before declaration, const cannot be reassigned and redeclared.
 
 // TODO 1.4 Try out the different arithmetic operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
 // Your code here
-
+x += 5;
+let y = 10;
+y /= 2;
+console.log(x);
+console.log(y);
 // Checkpoint 1.2 What operators did you use?
-// Answer: 
+// Answer: Addition and Division
 // Your code here
-
+// + for Addition and / for Division
 // TODO 1.5 Try out the different string operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
 // Your code here
-
+console.log("Hello" + "World");
+let text = "Hello";
+text += " World 2";
+console.log(text);
 // Checkpoint 1.3 What operators did you use?
-// Answer: 
+// Answer: plus sign for concatenation  
 
 // TODO 1.6 Try out the different logical operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
 // Your code here
-
+let a = 5;
+let b = 10;
+const result = a == 5 && b == 10;
+const result2 = a == 4 || b == 11;
+console.log(result);
+console.log(result2);
 // Checkpoint 1.4 What operators did you use?
-// Answer: 
+// Answer: logical AND and OR operators
 
 
 // TODO 1.7 Try out the different relational operators
--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -3,61 +3,90 @@
     - MDN Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling
 */
 
-const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock this value for testing
-
+const randomNumber = Math.floor(Math.random() * 100 + 1); // feel free to mock this value for testing
+console.log(randomNumber);
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
 // Your code here
-
+if (randomNumber > 50) {
+  console.log("more than fifty");
+}
 // TODO 2.2 Display whether the random number is odd or even
 // Your code here
-
+if (randomNumber % 2 == 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
 // Your code here
-
-let toDisplay = ""
+if (randomNumber % 3 == 0 && randomNumber % 5 == 0) {
+  console.log("fizzbuzz");
+} else if (randomNumber % 3 == 0) {
+  console.log("fizz");
+} else if (randomNumber % 5 == 0) {
+  console.log("buzz");
+} else {
+  console.log(randomNumber);
+}
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
 // Your code here
+let toDisplay = randomNumber % 2 == 0 ? "Even" : "Odd";
 console.log("toDisplay", toDisplay);
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
-// Answer: 
+// Answer: Use switch statements when you need to compare values and best to use for long if else statements
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
-const n = 10
+const n = 10;
 // Your code here
+for (let i = 1; i <= n; i++) {
+  console.log(i);
+}
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
 // Your code here
-
+let i = 0;
+while (i < list.length) {
+  console.log(list[i]);
+  i++;
+}
 // Checkpoint 2.3 What is the difference between do while and while loop?
-// Answer: 
+// Answer: the while loop evaluates the condition before the code block is executed, while the do-while loop evaluates the condition after the code block is executed.
 
 // TODO 2.7 Use a for of loop to display all the values in the list
 // Your code here
-
+for (let i = 0; i < list.length; i++) {
+  console.log(list[i]);
+}
 // TODO 2.8 Use a for in loop to display all the values in the list
 // Your code here
-
-// TODO 2.9 Use the for each method of the list to display all its values 
+for (let x in list) {
+  console.log(list[x]);
+}
+// TODO 2.9 Use the for each method of the list to display all its values
 // Your code here
-
+list.forEach((x) => {
+  console.log(x);
+});
 // Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
-// Answer: 
+// Answer:
 
 // TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
 // In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
-const numerator = Math.floor((Math.random() * 100) + 1);
-const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
-
-
-if(denominator === 0){
-    throw new Error("Division by zero error")
-} else {
-    console.log(numerator / denominator);
+try {
+  const numerator = Math.floor(Math.random() * 100 + 1);
+  const denominator = Math.floor(Math.random() * 5); // feel free to mock this value for testing
+  console.log(numerator / denominator);
 }
 
+const numerator = Math.floor(Math.random() * 100 + 1);
+const denominator = Math.floor(Math.random() * 5); // feel free to mock this value for testing
 
-
+if (denominator === 0) {
+  throw new Error("Division by zero error");
+} else {
+  console.log(numerator / denominator);
+}
